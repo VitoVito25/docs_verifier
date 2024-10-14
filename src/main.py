100e29601493dbc2eed9a869d4f6206e88d0eaea
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-10-14 08:02:02 -0300

    testando

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from util.UserInterface import *
 def main():
     interface = UserInterface                     ## Inicia o objeto de interface do usuario
     doc_names = DocNamesFile("docNamesFile.txt")  ## Inicia o objeto com o caminho do arquivo com os nomes
+    
     names_list = doc_names.read_names()
 
     interface.print_doc_names(names_list)
