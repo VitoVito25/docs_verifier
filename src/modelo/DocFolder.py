9e2409d636f18e098d9755388f176d10193167df
Victor Tomazoni Renaud <victortomazonir@gmail.com>
2024-10-16 10:25:52 -0300

    test_pdf_open()

--- src/modelo/DocFolder.py
+++ src/modelo/DocFolder.py
@@ -1,22 +1,49 @@
 from pathlib import Path
+import PyPDF2
 import os
 
 class DocFolder:
     def __init__(self, folder_path):
         self.folder_path = Path(folder_path)
 
-    def is_pdf(file_name):
+    def is_pdf(self, file_name):
         """
         Verifica se o arquivo possui a extensão .pdf.
         
         :param file_name: Nome do arquivo a ser verificado.
         :return: True se o arquivo for um PDF, False caso contrário.
-        
+
         """
         # Verifica se a extensão do arquivo é .pdf, ignorando diferenças de maiúsculas e minúsculas
         return os.path.splitext(file_name)[1].lower() == '.pdf'
 
 
+    def test_pdf_open(self, file_name):
+        """
+        Tenta abrir um arquivo PDF e verifica se ele está corrompido ou protegido.
+        
+        :param file_name: Nome do arquivo PDF (sem o caminho completo).
+        :return: True se o PDF pode ser aberto, False caso contrário.
+        """
+
+        # Constrói o caminho completo do arquivo PDF
+        file_path = self.folder_path / file_name
+        
+        try:
+            with open(file_path, 'rb') as file:
+                # Tenta ler o PDF usando PyPDF2
+                reader = PyPDF2.PdfReader(file)
+                
+                # Tenta acessar as páginas do PDF
+                if reader.pages:
+                    return True
+        except (PyPDF2.errors.PdfReadError, FileNotFoundError):
+            # Caso o PDF esteja corrompido ou não seja encontrado
+            return False
+        except Exception as e:
+            # Qualquer outro erro que impeça a abertura do arquivo
+            print(f"Erro ao abrir o arquivo: {e}")
+            return False
 
     def search_files(self, doc_names_list):
         try:
@@ -31,6 +58,20 @@ class DocFolder:
                 # Verifica se o nome do arquivo sem extensão está na lista fornecida
                 if file_without_extension in doc_names_list:
                     print(f"O arquivo '{file}' está na pasta!.")
+
+                    ## Verifica se esta corrompido
+                    if not self.is_pdf(file):
+                        print(f"O arquivo '{file}' não é um PDF.")
+                        continue
+
+                    if not self.test_pdf_open(file):
+                        print(f"O arquivo '{file}' esta corrompido ou protegido.")
+                        continue
+
+
+
+                    
+                    
 
         except FileNotFoundError:
             print(f"Erro: A pasta '{self.folder_path}' não foi encontrada.")
